c24e6b2131e38f430f4a1bada983433c33f2296e
jasperdifran <68628968+jasperdifran@users.noreply.github.com>
2022-10-05 14:52:52 +1100

    Clean config

--- ports/minimal/mpconfigport.h
+++ ports/minimal/mpconfigport.h
@@ -14,18 +14,13 @@
 #define MICROPY_MODULE_WEAK_LINKS               (1)
 #define MICROPY_ENABLE_GC                 (1)
 #define MICROPY_HELPER_REPL               (1)
-// #define MICROPY_MODULE_FROZEN_MPY         (1)
 #define MICROPY_PY_CMATH                        (1)
-// #define MICROPY_PY_UOS                    (1)
 #define MICROPY_ENABLE_EXTERNAL_IMPORT    (1)
-// #define MICROPY_PY_SYS_STDFILES            (1)
 #define MICROPY_PY_FSTRINGS         (1) 
 #define MICROPY_PY_SYS              (1)
 #define MICROPY_PY_BUILTINS_BYTEARRAY   (1)
 #define MICROPY_PY_BUILTINS_SLICE   (1)
 #define MICROPY_CPYTHON_COMPAT      (1)
-// #define MICROPY_PY_UASYNCIO         (1)
-// #define MICROPY_PY_SOCKET           (1)
 
 
 #define MICROPY_ALLOC_PATH_MAX            (256)
