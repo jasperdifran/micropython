c9e5e3b91a3c0629f8fc62de07a80101121f7770
jasperdifran <68628968+jasperdifran@users.noreply.github.com>
2023-03-29 19:13:13 +1100

    Testing minimal port

--- ports/minimal/subfiles/websrv.py
+++ ports/minimal/subfiles/websrv.py
@@ -1,97 +1,97 @@
+exit(1)
+# from phew import server
+# from phew.stream import Reader
 
-from phew import server
-from phew.stream import Reader
+# from metadata.breadcrumbs import breadcrumbs
+# from metadata.nav import nav
 
-from metadata.breadcrumbs import breadcrumbs
-from metadata.nav import nav
+# from phew.stream import p
+# import cptxrx
+# import cpfs as fs
+# from sDDF.echo_server.micropython.ports.minimal.subfiles.phew.server import FileResponse
 
-from phew.stream import p
-import cptxrx
-import cpfs as fs
-from sDDF.echo_server.micropython.ports.minimal.subfiles.phew.server import FileResponse
-
-def make_breadcrumbs(page_name):
-  # We assume that the page exists
-  pages = page_name.split('/')
-  crumbs = []
-  if len(pages) == 0:
-    return []
-  else:
-    crumbs = breadcrumbs[pages[0]][pages[1]]
-  print("Crumbs", crumbs)
-  res = b"\n<ul>\n"
-  for item in crumbs:
-    res += b'\t<li href="' + item['url'] + b'">' + item['name'] + b'</li>\n'
-  res += b'</ul>\n'
-  return res
-
-
-def render_nav():
-  res = b'<ul>'
-  for item in nav:
-    res += b'<li><a href="' + item['url'] + b'">' + item['name'] + b'</a></li>'
-    if (item['children'] != None):
-      res += b'<ul>'
-      for child in item['children']:
-        res += b'<li><a href="' + child['url'] + b'">' + child['name'] + b'</a></li>'
-      res += b'</ul>'
-  res += b'</ul>'
-  return res
-
-def page_response(page_name, addBreadcrumbs=True):
-  content = b''
-  content += fs.readfile('/templates/sel4header.html')
-  content += render_nav()
-  if addBreadcrumbs:
-    content += make_breadcrumbs(page_name)
-  content += fs.readfile('/content/' + page_name + '.html')
-  content += fs.readfile('/templates/sel4footer.html')
-
-  return content
+# def make_breadcrumbs(page_name):
+#   # We assume that the page exists
+#   pages = page_name.split('/')
+#   crumbs = []
+#   if len(pages) == 0:
+#     return []
+#   else:
+#     crumbs = breadcrumbs[pages[0]][pages[1]]
+#   print("Crumbs", crumbs)
+#   res = b"\n<ul>\n"
+#   for item in crumbs:
+#     res += b'\t<li href="' + item['url'] + b'">' + item['name'] + b'</li>\n'
+#   res += b'</ul>\n'
+#   return res
 
 
+# def render_nav():
+#   res = b'<ul>'
+#   for item in nav:
+#     res += b'<li><a href="' + item['url'] + b'">' + item['name'] + b'</a></li>'
+#     if (item['children'] != None):
+#       res += b'<ul>'
+#       for child in item['children']:
+#         res += b'<li><a href="' + child['url'] + b'">' + child['name'] + b'</a></li>'
+#       res += b'</ul>'
+#   res += b'</ul>'
+#   return res
+
+# def page_response(page_name, addBreadcrumbs=True):
+#   content = b''
+#   content += fs.readfile('/templates/sel4header.html')
+#   content += render_nav()
+#   if addBreadcrumbs:
+#     content += make_breadcrumbs(page_name)
+#   content += fs.readfile('/content/' + page_name + '.html')
+#   content += fs.readfile('/templates/sel4footer.html')
+
+#   return content
 
 
 
-@server.route('/', methods=['GET'])
-def index(request):
-  return page_response('index', addBreadcrumbs=False), 200, 'text/html'
-
-@server.route('/[page_name]', methods=['GET'])
-def page(request, page_name):
-  p("Page name", page_name)
-  if '.' in page_name:
-    return "Not found", 404
-  if page_name == '':
-    page_name = 'index'
-  stat = fs.stat("content/" + page_name + ".html")
-  if isinstance(stat, int):
-    stat = fs.stat("content/" + page_name)
-  if isinstance(stat, int):
-    return "Not found", 404
-  elif stat[3] == 1:
-    page_name = page_name + "/index"
-  return page_response(page_name), 200, 'text/html'
 
 
-@server.route('/css/[file_name]', methods=['GET'])
-def css(request, file_name):
-  return FileResponse('content/css/' + file_name)
+# @server.route('/', methods=['GET'])
+# def index(request):
+#   return page_response('index', addBreadcrumbs=False), 200, 'text/html'
 
-# @server.route("/[page_name]", methods=["GET"])
-# def page_server(request, page_name):
-#   if (not template_exists(page_name)):
+# @server.route('/[page_name]', methods=['GET'])
+# def page(request, page_name):
+#   p("Page name", page_name)
+#   if '.' in page_name:
 #     return "Not found", 404
-#   a = b"".join([x for x in render_template("sel4template", page_name=page_name)])
-#   return a, 200, "text/html"
+#   if page_name == '':
+#     page_name = 'index'
+#   stat = fs.stat("content/" + page_name + ".html")
+#   if isinstance(stat, int):
+#     stat = fs.stat("content/" + page_name)
+#   if isinstance(stat, int):
+#     return "Not found", 404
+#   elif stat[3] == 1:
+#     page_name = page_name + "/index"
+#   return page_response(page_name), 200, 'text/html'
 
-# catchall example
-@server.catchall()
-def catchall(request):
-  return "Not found", 404
 
-from phew.stream import Reader, Writer
-from phew.server import handle_request
+# @server.route('/css/[file_name]', methods=['GET'])
+# def css(request, file_name):
+#   return FileResponse('content/css/' + file_name)
 
-writer = Writer()
-handle_request(Reader(cptxrx.rx()), writer)
+# # @server.route("/[page_name]", methods=["GET"])
+# # def page_server(request, page_name):
+# #   if (not template_exists(page_name)):
+# #     return "Not found", 404
+# #   a = b"".join([x for x in render_template("sel4template", page_name=page_name)])
+# #   return a, 200, "text/html"
+
+# # catchall example
+# @server.catchall()
+# def catchall(request):
+#   return "Not found", 404
+
+# from phew.stream import Reader, Writer
+# from phew.server import handle_request
+
+# writer = Writer()
+# handle_request(Reader(cptxrx.rx()), writer)
--- ports/minimal/uart_core.c
+++ ports/minimal/uart_core.c
@@ -1,12 +1,14 @@
 #include <unistd.h>
 #include "py/mpconfig.h"
+#include <stdio.h>
 
 /*
  * Core UART functions to implement for a port
  */
 
 #if MICROPY_MIN_USE_STM32_MCU
-typedef struct {
+typedef struct
+{
     volatile uint32_t SR;
     volatile uint32_t DR;
 } periph_uart_t;
@@ -14,31 +16,36 @@ typedef struct {
 #endif
 
 // Receive single character
-int mp_hal_stdin_rx_chr(void) {
+int mp_hal_stdin_rx_chr(void)
+{
     unsigned char c = 0;
-    #if MICROPY_MIN_USE_STDOUT
+#if MICROPY_MIN_USE_STDOUT
     int r = read(STDIN_FILENO, &c, 1);
     (void)r;
-    #elif MICROPY_MIN_USE_STM32_MCU
+#elif MICROPY_MIN_USE_STM32_MCU
     // wait for RXNE
-    while ((USART1->SR & (1 << 5)) == 0) {
+    while ((USART1->SR & (1 << 5)) == 0)
+    {
     }
     c = USART1->DR;
-    #endif
+#endif
     return c;
 }
 
 // Send string of given length
-void mp_hal_stdout_tx_strn(const char *str, mp_uint_t len) {
-    #if MICROPY_MIN_USE_STDOUT
+void mp_hal_stdout_tx_strn(const char *str, mp_uint_t len)
+{
+#if MICROPY_MIN_USE_STDOUT
     int r = write(STDOUT_FILENO, str, len);
     (void)r;
-    #elif MICROPY_MIN_USE_STM32_MCU
-    while (len--) {
+#elif MICROPY_MIN_USE_STM32_MCU
+    while (len--)
+    {
         // wait for TXE
-        while ((USART1->SR & (1 << 7)) == 0) {
+        while ((USART1->SR & (1 << 7)) == 0)
+        {
         }
         USART1->DR = *str++;
     }
-    #endif
+#endif
 }
